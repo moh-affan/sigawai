3ad6897f1889c95c10542b554499f76fa1915239
Mohammad Affan <b4affan@gmail.com>
2019-08-05 09:19:17 +0700

    test login

--- assets/js/lib/pages/Login.js
+++ assets/js/lib/pages/Login.js
@@ -24,9 +24,11 @@ define(function (require) {
             if (next === null)
                 next = '/';
             let that = this;
-            let data = {'username': this.inputUsername.current.value, 'password': this.inputPassword.current.value};
+            let data = { 'username': this.inputUsername.current.value, 'password': this.inputPassword.current.value };
             axios.post('/login', data, config)
                 .then(function (response) {
+                    console.log(response);
+                    return;
                     if (typeof response.data.username !== 'undefined')
                         window.location.href = next;
                     else
@@ -50,7 +52,7 @@ define(function (require) {
                                 <div className="card px-4 py-2 box-shadow-2 width-400">
                                     <div className="card-header text-center">
                                         <img src="/public/img/korpri.png" alt="company-logo"
-                                             className="mb-3" width="80"/>
+                                            className="mb-3" width="80" />
                                         <h4 className="text-uppercase text-bold-500 blue darken-1"><span
                                             className="text-success">Si</span>Gawai</h4>
                                         <h4 className="text-uppercase text-bold-400 grey darken-1">Login</h4>
@@ -61,17 +63,17 @@ define(function (require) {
                                                 <div className="form-group">
                                                     <div className="col-md-12">
                                                         <input type="text" className="form-control form-control-lg"
-                                                               name="username" id="username" ref={this.inputUsername}
-                                                               placeholder="Username" required/>
+                                                            name="username" id="username" ref={this.inputUsername}
+                                                            placeholder="Username" required />
                                                     </div>
                                                 </div>
 
                                                 <div className="form-group">
                                                     <div className="col-md-12">
                                                         <input type="password" className="form-control form-control-lg"
-                                                               name="password" id="password" ref={this.inputPassword}
-                                                               placeholder="Password"
-                                                               required/>
+                                                            name="password" id="password" ref={this.inputPassword}
+                                                            placeholder="Password"
+                                                            required />
                                                     </div>
                                                 </div>
 
@@ -86,8 +88,8 @@ define(function (require) {
                                                 <div className="form-group">
                                                     <div className="text-center col-md-12">
                                                         <button type="button"
-                                                                onClick={this.login}
-                                                                className="btn btn-danger px-4 py-2 text-uppercase white font-small-4 box-shadow-2 border-0">Login
+                                                            onClick={this.login}
+                                                            className="btn btn-danger px-4 py-2 text-uppercase white font-small-4 box-shadow-2 border-0">Login
                                                         </button>
                                                     </div>
                                                 </div>
