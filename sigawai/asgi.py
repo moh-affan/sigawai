2c7b494b1a6dabebeab22ce04609ef759d8f7eb7
Mohammad Affan <b4affan@gmail.com>
2019-08-08 07:47:00 +0700

    https on

--- sigawai/asgi.py
+++ sigawai/asgi.py
@@ -3,5 +3,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sigawai.settings")
+os.environ['HTTPS'] = 'on'
 django.setup()
 application = get_default_application()
